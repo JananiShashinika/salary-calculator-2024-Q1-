d5a9382f7077fb5ae9a74698745550dcea889fc0
JananiShashinika <s17394@sci.pdn.ac.lk>
2024-06-02 13:27:39 +0530

    Update Main.jsx - fourth commit

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -4,18 +4,23 @@ import CalculateForm from './CalculateForm';
 import PaySheet from './PaySheet';
 
 export default function Main() {
+  // Define results state and a function to update it
   const [results, setResults] = useState(null);
 
   return (
     <div>
       <div className='main'>
         <div className="container1">
+
+        
           <div className='h4 title'>Calculate Your Salary</div>
-          <img type='button' src='Pages/Images/_Link.png' className='linkImage' />
+          <img type='reset' src='Pages/Images/_Link.png' className='linkImage' />
+          {/* Pass results state and setResults function as props to CalculateForm */}
           <CalculateForm setResults={setResults} />
         </div>
 
         <div className='container2'>
+          {/* Pass results state to PaySheet */}
           <PaySheet results={results} />
         </div>
       </div>
